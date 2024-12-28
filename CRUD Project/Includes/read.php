e3ef69c0bb19a8aa7d532a4541be5a7cfe293d00
sara-c9 <sara.caputo.1994@gmail.com>
2024-12-28 11:39:05 +0000

    small changes

--- CRUD Project/Includes/read.php
+++ CRUD Project/Includes/read.php
@@ -15,7 +15,6 @@ if (@mysqli_num_rows($r) != 0) {
         echo'<tr>
         <td>'.$row['item_id'].'</td><td>'.$row['item_name'].'</td><td>'.$row['item_desc'].'</td><td><img src="'.$row['item_img'].'" alt="product" width="50" heigh="50"></td><td>&pound'.$row['item_price'].'</td><td>';
     }
-    echo'</tr><table><br><br><a href="create.php">Add Records  </a><a href="read.php">Read Records  </a><a href="update.php">Update Records  </a> <a href="delete.php">Delete Records</a>';
 }
 
 mysqli_close($link);

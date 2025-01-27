0adb724d92553d490149f9813212d97ed378d2bb
Sara-c9 <sara.caputo.1994@gmail.com>
2025-01-27 18:18:15 +0000

    Delete MKTIME directory

--- MKTIME/connection_db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$link = mysqli_connect('localhost','root','','codespace'); 
-if (!$link) { 
-die('Could not connect to MySQL: ' . mysqli_error()); 
-} 
-// echo 'Connected to the database successfully!';  
-?>
